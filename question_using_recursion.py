a0204aeb0058cb7d1c500bc61aa82e927fad8425
errorboy <69593024+errorboy4O4@users.noreply.github.com>
2023-02-15 17:10:35 +0530

    Update question_using_recursion.py

--- question_using_recursion.py
+++ question_using_recursion.py
@@ -8,3 +8,41 @@ def power(n):
 	return 2 * power(n-1)
 a = power(5)
 print(a)
+
+
+####### Print Counting ###### 
+##### Using Recursion ######
+
+def counting(n):
+	if n == 0:
+		return 
+	print(n)
+	return counting(n-1)	
+a = counting(5)
+print(a)   
+
+
+
+####### FIBONACCI NUMBER ###### 
+##### Using Recursion ######
+
+def fib(n):
+	if n == 0:
+		return 0
+	if n == 1:
+		return 1
+	return fib(n-1) + fib(n-2)
+a = fib(4)
+print(a)
+
+
+
+
+
+
+
+
+
+
+
+
